b5709b0f0eff9aef8a945cd6eb133c740643f954
vovantuan2002 <votuant2@gmail.com>
2022-05-27 13:40:08 +0700

    first commit

---
+++ app/Models/ProductAttributeItem.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductAttributeItem extends Model
+{
+    use HasFactory;
+    protected $table = 'product_attribute_items';
+}
---
+++ app/Models/ProductProductAttribute.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductProductAttribute extends Model
+{
+    use HasFactory;
+    protected $table = 'product_product_attributes';
+}
